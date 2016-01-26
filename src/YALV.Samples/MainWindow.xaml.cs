873631de302e000a98876788a3c3918ef84f48e5
Michalis Nicolaides <michael1337@hotmail.com>
2016-01-26 23:16:45 +0200

    Renamed methods and exception messages to reflect their functionality

--- src/YALV.Samples/MainWindow.xaml.cs
+++ src/YALV.Samples/MainWindow.xaml.cs
@@ -37,53 +37,53 @@ namespace YALV.Samples
                     case 1:
                     case 2:
                     case 3:
-                        method1();
+                        GenerateDebugTrace();
                         break;
                     case 4:
                     case 5:
                     case 6:
                     case 7:
                     case 8:
-                        method2();
+                        GenerateInfoTrace();
                         break;
                     case 9:
-                        method3();
+                        GenerateWarningTrace();
                         break;
                     case 10:
                     case 11:
-                        method4();
+                        GenerateErrorTrace();
                         break;
                     case 12:
-                        method5();
+                        GenerateFatalTrace();
                         break;
                 }
             }
             MessageBox.Show("Generation Complete!", "YALV! Samples", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
-        private void method1()
+        private void GenerateDebugTrace()
         {
             LogService.Trace.Debug("This is a debug message");
         }
 
-        private void method2()
+        private void GenerateInfoTrace()
         {
             LogService.Trace.Info("This is an information message");
         }
 
-        private void method3()
+        private void GenerateWarningTrace()
         {
             LogService.Trace.Warn("This is a warning message!", new Exception("Warning Exception!"));
         }
 
-        private void method4()
+        private void GenerateErrorTrace()
         {
-            LogService.Trace.Error("This is an error message!", new Exception("Warning Exception!"));
+            LogService.Trace.Error("This is an error message!", new Exception("Error Exception!"));
         }
 
-        private void method5()
+        private void GenerateFatalTrace()
         {
-            LogService.Trace.Fatal("This is a fatal message!", new Exception("Warning Exception!"));
+            LogService.Trace.Fatal("This is a fatal message!", new Exception("Fatal Exception!"));
         }
     }
 }
